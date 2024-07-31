66c2f91890686bd7b5b9b9853476e9468f85c29e
m4rcosmatos <m4rcosff@gmail.com>
2024-07-30 21:15:39 -0300

    Estrutura base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import './App.css'
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+import "./App.css";
 
 function App() {
-
-
   return (
-    <div className= "App">
-      <h1>React Multistep</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulario abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious/>
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext/>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
