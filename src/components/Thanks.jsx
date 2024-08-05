99a9f469c2811665a150968a4743c41fb751c749
m4rcosmatos <m4rcosff@gmail.com>
2024-08-05 20:46:50 -0300

    Componente de envio de avaliação

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./Thanks.css";
 
 const Thanks = () => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você recebera um cupom de 10%
+        de desconto para sua proxima compra.
+      </p>
+      <p>Para concluir a sua avaliação clique no botao de enviar abaixo </p>
+      <h3>Aqui esta o resumo da sua avaliaçao</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentario:</span>
+      </p>
     </div>
   );
 };
